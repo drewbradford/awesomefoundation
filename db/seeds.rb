8b6a8d5194dc551213135bd1248bfec10ec96394
Jesse Farmer <jesse@everlane.com>
2011-04-23 14:26:47 -0700

    Update seeds.rb style

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 chapter = Chapter.create! :name => 'Atlantis', :slug => 'atlantis', :tagline => "It's hotter under de water", :body => "This is a development chapter."
 
 # Create a sample user
-user = User.new(:login => "Poseidon", :first_name => "Poseidon", :last_name => "Johnson", :password => "password", :password_confirmation => "password", :chapter_id => chapter.id, :admin => true)
+user = User.new(:login => "Poseidon", :first_name => "Poseidon", :last_name => "Johnson", :password => "password", :password_confirmation => "password", :chapter_id => chapter.id)
 user.admin = true
 user.save
