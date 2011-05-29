9c96d18a04eb13a639585e52fe5a1897c883a8ab
Jesse Chan-Norris <jcn@pith.org>
2011-05-29 13:13:48 -0400

    Notification formatting updates

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
   def submission_notification(submission)
     recipients submission.email
-    from       "contact@awesomefoundation.org"
+    from       "The Awesome Foundation <contact@awesomefoundation.org>"
     subject    "Your Awesome Foundation Application!"
     body       :submission => submission
   end
